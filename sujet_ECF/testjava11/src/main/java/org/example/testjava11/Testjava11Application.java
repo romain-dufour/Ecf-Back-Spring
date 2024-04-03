6cf3bed86c091dca629413799ac42791499c8d8d
romain <rdj.dufour@gmail.com>
2024-04-03 12:23:25 +0200

    repo ecf suite

---
+++ sujet_ECF/testjava11/src/main/java/org/example/testjava11/Testjava11Application.java
@@ -0,0 +1,13 @@
+package org.example.testjava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testjava11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testjava11Application.class, args);
+    }
+
+}
---
+++ sujet_ECF/testjava11/src/test/java/org/example/testjava11/Testjava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testjava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testjava11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
